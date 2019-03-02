465f11b48c7050fac90750b46b02f827e3b8b990
Tolga <htolgasahin@gmail.com>
2019-03-02 22:44:44 +0100

    Build fix

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,7 +5,6 @@ module.exports = {
     themeConfig: {
         sidebar: [
             ["/", "Home"],
-            ["/cv.html", "Resume"],
             ["linux/2019-03-02-how-i-setup-my-pi.html", "Starting with RaspberryPi"]
         ],
     //   logo: '/assets/img/logo.png',
