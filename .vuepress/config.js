cc894572834d33db3903e5a4804db2ef06303d26
Hilmi Tolga Sahin <hilmisahin@pvh.com>
2019-03-20 14:26:56 +0100

    Add gpg git commit signing to sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,7 +5,8 @@ module.exports = {
     themeConfig: {
         sidebar: [
             ["/", "Home"],
-            ["linux/2019-03-02-how-i-setup-my-pi.html", "Starting with RaspberryPi"]
+            ["linux/2019-03-02-how-i-setup-my-pi.html", "Starting with RaspberryPi"],
+            ["linux/2019-03-20-gpg-signing.html", "Signing git commits with GPG"]
         ],
     //   logo: '/assets/img/logo.png',
     }
