aecfcba8e3e010e3c456ac215e179c2d0b5158c1
Tolga <htolgasahin@gmail.com>
2019-04-11 23:45:41 +0200

    Added bundler mystery javascript post

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,8 +6,12 @@ module.exports = {
         sidebar: [
             ["/", "Home"],
             ["linux/2019-03-02-how-i-setup-my-pi.html", "Starting with RaspberryPi"],
-            ["linux/2019-03-20-gpg-signing.html", "Signing git commits with GPG"]
+            ["linux/2019-03-20-gpg-signing.html", "Signing git commits with GPG"],
+            ["javascript/2019-04-11-node-core-modules-on-browser.html", "Native node modules on browser"],
         ],
     //   logo: '/assets/img/logo.png',
+    },
+    markdown: {
+        linenumbers: true
     }
   }
